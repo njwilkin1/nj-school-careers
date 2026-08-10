3654c1195ee04b1dfef1781a9a1f2f873cc2f7b1
njwilkin <njwilkin@hotmail.com>
2026-08-09 22:04:45 -0400

    Fix Stripe test webhook authentication

--- app/api/stripe-webhook-test/route.ts
+++ app/api/stripe-webhook-test/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
-
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+const stripe = new Stripe(process.env.STRIPE_TEST_SECRET_KEY!);
 
 export async function POST(req: Request) {
   const body = await req.text();
